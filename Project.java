80888cce73f4d576c02f1f5229bf99d2c3d5a190
Chandramani Patel <apatel4328@gmail.com>
2026-04-13 21:42:24 +0530

    2nd commit

--- Project.java
+++ Project.java
@@ -1,8 +1,19 @@
 class Wallet{
+    private double balance;
+    static double cashback = 0.02;
 
+    double GettergetBalance(){
+        return balance;
+    }
+
+    void deposite(double amount){
+        if(amount > 0){
+            balance += amount;
+        }
+    }
 }
 public class Project{
     public static void main(String[] args){
-
+        
     }
 }
