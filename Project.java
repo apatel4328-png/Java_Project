3fd7a85d05ea9c19610eee5def4a385af024d45a
Chandramani Patel <apatel4328@gmail.com>
2026-06-03 19:04:06 +0530

    Updated code

--- Project.java
+++ Project.java
@@ -1,21 +1,61 @@
+import java.util.*;
 class Wallet{
     private double balance;
-    static double cashback = 0.02;
+    static double cashbackRate = 0.02;
 
-    double GettergetBalance(){
+    public double getBalance(){
         return balance;
     }
 
-    void deposit(double amount){
+    public void deposit(double amount){
         if(amount > 0){
             balance += amount;
+        }
+    }
+    public void transferTo(Wallet receiver, double amount) {
+        try {
+            if (amount <= 0) {
+                throw new Exception("Invalid amount");
+            }
+
+            if (balance < amount) {
+                throw new Exception("Insufficient balance");
+            }
+
+            balance -= amount;
+            receiver.balance += amount;
+
+            double cashback = amount * cashbackRate;
+            balance += cashback;
+
+            System.out.println("Transaction successful.");
+            System.out.println("Cashback received: " + cashback);
+
+        } catch (Exception e) {
+            System.out.println("Transaction failed: " + e.getMessage());
         }
     }
 }
 public class Project{
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        double depositAmount = sc.nextDouble();
+        double transferAmount = sc.nextDouble();
         Wallet w1 = new Wallet();
         Wallet w2 = new Wallet();
-        w1.deposit(1000);
+        
+        w1.deposit(depositAmount);
+
+        System.out.println("Initial Balance (W1): " + w1.getBalance());
+        
+
+        
+        System.out.println("Transferring amount...");
+        System.out.println();
+        w1.transferTo(w2, transferAmount);
+
+        System.out.println();
+        System.out.println("Final Balance (W1): " + w1.getBalance());
+        System.out.println("Final Balance (W2): " + w2.getBalance());
     }
 }
